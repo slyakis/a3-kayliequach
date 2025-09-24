396630afb39cfb80deb454a2d1f80f5c4d2b642d
Kaylie Quach <97192455+slyakis@users.noreply.github.com>
2025-09-23 21:26:54 -0400

    commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ passport.deserializeUser((obj, done) => {
 passport.use(new GitHubStrategy({
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/github/callback"
+      callbackURL: "https://a3-kayliequach.vercel.app/auth/github/callback"
     },
     function(accessToken, refreshToken, profile, cb) {
         console.log("GitHub profile:", profile)
